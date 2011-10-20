b8cd8441252f86e7950ff439c4e81ffceaaacb8d
Mikhail Savkin <savkinmk@gmail.com>
2011-10-20 13:01:36 +0400

    Sources were added

---
+++ sources/GTRunner/Forms/Form1.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Forms;
+
+namespace GTRunner.Forms
+{
+	public partial class MainForm : Form
+	{
+		#region Constructors
+
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		#endregion Constructors
+	}
+}
---
+++ sources/GTRunner/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+using GTRunner.Forms;
+
+namespace GTRunner
+{
+	static class Program
+	{
+		#region Main
+
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+
+		#endregion Main
+	}
+}
